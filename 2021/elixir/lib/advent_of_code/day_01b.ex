21a02a9d17a95478958a5199945dd04d488fc37b
Mackenzie Morgan <macoafi@gmail.com>
2021-12-01 23:06:15 -0500

    make chunk_every solution more generic

--- 2021/elixir/lib/advent_of_code/day_01b.ex
+++ 2021/elixir/lib/advent_of_code/day_01b.ex
@@ -1,22 +1,26 @@
 defmodule AdventOfCode.Day01b do
 @moduledoc """
   But I also wanted to try it out with Enum.chunk_every/4
+
+  ...and then Akash on Elixir Slack asked about
+  handling generic windows, and I made this DRYer
 """
   def part1(args) do
-    args
-    |> String.trim()
-    |> String.split("\n")
-    |> Enum.map(& String.to_integer/1)
-    |> Enum.chunk_every(2, 1, :discard)
-    |> Enum.count(fn [a, b] -> a < b end)
+    compare_window(args, 2)
   end
 
   def part2(args) do
-    args
+    compare_window(args, 4)
+  end
+
+  defp compare_window(input, size) do
+    input
     |> String.trim()
     |> String.split("\n")
     |> Enum.map(& String.to_integer/1)
-    |> Enum.chunk_every(4, 1, :discard)
-    |> Enum.count(fn [a, _b, _c, d] -> a < d end)
+    |> Enum.chunk_every(size, 1, :discard)
+    |> Enum.count(fn list ->
+      List.first(list) < List.last(list)
+    end)
   end
 end
