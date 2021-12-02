10fe08d84f6c7a4a913fe0db2d260625a7d6d280
Mackenzie Morgan <macoafi@gmail.com>
2021-12-02 00:32:29 -0500

    day 2

--- 2021/elixir/lib/advent_of_code/day_02.ex
+++ 2021/elixir/lib/advent_of_code/day_02.ex
@@ -1,7 +1,45 @@
 defmodule AdventOfCode.Day02 do
   def part1(args) do
+    {horizontal, depth} =
+      args
+      |> parse()
+      |> Enum.reduce({0, 0}, fn
+        ["up", distance], {horizontal, depth} ->
+          {horizontal, depth - distance}
+
+        ["down", distance], {horizontal, depth} ->
+          {horizontal, depth + distance}
+
+        ["forward", distance], {horizontal, depth} ->
+          {horizontal + distance, depth}
+      end)
+
+    horizontal * depth
   end
 
   def part2(args) do
+    {horizontal, depth, _aim} =
+      args
+      |> parse()
+      |> Enum.reduce({0, 0, 0}, fn
+        ["down", distance], {horizontal, depth, aim} ->
+          {horizontal, depth, aim + distance}
+
+        ["up", distance], {horizontal, depth, aim} ->
+          {horizontal, depth, aim - distance}
+
+        ["forward", distance], {horizontal, depth, aim} ->
+          {horizontal + distance, depth + aim * distance, aim}
+      end)
+
+    horizontal * depth
+  end
+
+  defp parse(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&String.split(&1, " "))
+    |> Enum.map(fn [direction, num] -> [direction, String.to_integer(num)] end)
   end
 end
