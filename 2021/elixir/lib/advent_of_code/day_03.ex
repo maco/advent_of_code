7d228b0641e67f617c9c792f5e0bd5e8c6c56849
Mackenzie Morgan <macoafi@gmail.com>
2021-12-03 00:42:49 -0500

    day 3 part 1

--- 2021/elixir/lib/advent_of_code/day_03.ex
+++ 2021/elixir/lib/advent_of_code/day_03.ex
@@ -1,7 +1,41 @@
 defmodule AdventOfCode.Day03 do
   def part1(args) do
+    parsed = parse(args)
+    entries = Enum.count(parsed)
+    gamma_list =
+      parsed
+      |> Enum.zip()
+      |> Enum.map(fn t ->
+         ones =
+         Tuple.to_list(t)
+          |> Enum.count(& &1 == "1")
+          if ones > (entries/2) do
+            "1"
+          else
+            "0"
+          end
+      end)
+
+
+    gamma = gamma_list
+    |> Enum.join()
+    |> String.to_integer(2)
+
+    epsilon = String.pad_leading("", Enum.count(gamma_list), "1")
+    |> String.to_integer(2)
+    |> Bitwise.bxor(gamma)
+
+    gamma * epsilon
+
   end
 
   def part2(args) do
+  end
+
+  defp parse(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&String.graphemes/1)
   end
 end
