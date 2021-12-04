4a0116ed78d1041c19f4e7da83e6cb306150fed7
Mackenzie Morgan <macoafi@gmail.com>
2021-12-04 01:35:02 -0500

    day 4 part 1

--- 2021/elixir/lib/advent_of_code/day_04.ex
+++ 2021/elixir/lib/advent_of_code/day_04.ex
@@ -1,5 +1,174 @@
 defmodule AdventOfCode.Day04 do
   def part1(args) do
+    {numbers, boards} = String.split(args, "\n", trim: true) |> numbers_and_boards()
+
+    {[winner | _], n} = Enum.reduce_while(numbers, boards, fn n, b ->
+      {updated, winners} = play_number(n, b)
+      if Enum.count(winners) > 0 do
+        {:halt, {winners, n}}
+      else
+        {:cont, updated}
+      end
+    end)
+
+    score_board(winner) * n
+  end
+
+  def play_number(n, boards) do
+    new_boards = Enum.map(boards, &update_board(n, &1))
+    winning_boards = Enum.filter(new_boards, &is_winner(&1))
+    {new_boards, winning_boards}
+  end
+
+  @doc"""
+      iex> AdventOfCode.Day04.update_board(13, [
+      ...> [[22, false], [13, false], [17, false], [11, false], [0, false]],
+      ...> [[8, true], [2, true], [23, true], [4, true], [24, true]],
+      ...> [[21, false], [9, false], [14, false], [16, false], [7, false]],
+      ...> [[6, false], [10, false], [3, false], [18, false], [5, false]],
+      ...> [[1, false], [12, false], [20, false], [15, false], [19, false]]])
+      [
+      [[22, false], [13, true], [17, false], [11, false], [0, false]],
+      [[8, true], [2, true], [23, true], [4, true], [24, true]],
+      [[21, false], [9, false], [14, false], [16, false], [7, false]],
+      [[6, false], [10, false], [3, false], [18, false], [5, false]],
+      [[1, false], [12, false], [20, false], [15, false], [19, false]]]
+  """
+  def update_board(num, board) do
+    Enum.map(board, &update_row(num, &1))
+  end
+
+  @doc"""
+      iex> AdventOfCode.Day04.update_row(4, [[8, false], [2, false], [23, false], [4, false], [24, false]])
+      [[8, false], [2, false], [23, false], [4, true], [24, false]]
+  """
+  def update_row(num, row) do
+    # for [^num, _] <- row, do: [num, true]
+    Enum.map(row, fn
+      [^num, _] -> [num, true]
+      [x, val] -> [x, val]
+    end)
+  end
+
+  @doc"""
+      iex> AdventOfCode.Day04.score_board([
+      ...> [[22, false], [13, false], [17, false], [11, false], [0, false]],
+      ...> [[8, true], [2, true], [23, true], [4, true], [24, true]],
+      ...> [[21, false], [9, false], [14, false], [16, false], [7, false]],
+      ...> [[6, false], [10, false], [3, false], [18, false], [5, false]],
+      ...> [[1, false], [12, false], [20, false], [15, false], [19, false]]])
+      239
+  """
+  def score_board(board) do
+    List.flatten(board)
+    |> Enum.chunk_every(2)
+    |> Enum.reduce(0, fn
+      [_x, true], acc ->
+        acc
+      [x, false], acc ->
+        acc + x
+      end)
+  end
+
+  @doc"""
+      iex> AdventOfCode.Day04.is_winner([[[22, false], [13, false], [17, false], [11, false], [0, false]],
+      ...> [[8, true], [2, true], [23, true], [4, true], [24, true]],
+      ...> [[21, false], [9, false], [14, false], [16, false], [7, false]],
+      ...> [[6, false], [10, false], [3, false], [18, false], [5, false]],
+      ...> [[1, false], [12, false], [20, false], [15, false], [19, false]]])
+      true
+
+      iex> AdventOfCode.Day04.is_winner([[[22, false], [13, false], [17, false], [11, false], [0, true]],
+      ...> [[8, false], [2, false], [23, false], [4, false], [24, true]],
+      ...> [[21, false], [9, false], [14, false], [16, false], [7, true]],
+      ...> [[6, false], [10, false], [3, false], [18, false], [5, true]],
+      ...> [[1, false], [12, false], [20, false], [15, false], [19, true]]])
+      true
+
+      iex> AdventOfCode.Day04.is_winner([[[22, false], [13, false], [17, false], [11, false], [0, true]],
+      ...> [[8, false], [2, false], [23, false], [4, false], [24, true]],
+      ...> [[21, false], [9, false], [14, false], [16, false], [7, true]],
+      ...> [[6, false], [10, false], [3, false], [18, false], [5, true]],
+      ...> [[1, false], [12, false], [20, false], [15, false], [19, false]]])
+      false
+
+  """
+  def is_winner(board) do
+    check_rows(board) or check_columns(board)
+  end
+
+  @doc"""
+      iex> AdventOfCode.Day04.check_row([[8, true], [2, true], [23, true], [4, true], [24, true]])
+      true
+
+      iex> AdventOfCode.Day04.check_row([[8, true], [2, true], [23, true], [4, false], [24, true]])
+      false
+  """
+  def check_row(row) do
+    matching = for [r, true] <- row, do: r
+    Enum.count(matching) == Enum.count(row)
+  end
+
+  @doc"""
+      iex> AdventOfCode.Day04.check_rows([[[8, false], [2, true], [23, true], [4, true], [24, true]],
+      ...> [[8, true], [2, true], [23, true], [4, true], [24, true]]])
+      true
+
+      iex> AdventOfCode.Day04.check_rows([[[8, true], [2, false], [23, true], [4, false], [24, true]],
+      ...> [[8, true], [2, true], [23, true], [4, false], [24, true]]])
+      false
+
+  """
+  def check_rows(rows) do
+    Enum.map(rows, &check_row/1) |> Enum.any?()
+  end
+
+  @doc"""
+      iex> AdventOfCode.Day04.check_columns(
+      ...> [[[8, true], [2, false], [23, false]],
+      ...> [[4, true], [24, false], [25, false]],
+      ...> [[8, true], [2, false], [23, false]],
+      ...> [[4, true], [24, false], [25, false]]])
+      true
+
+      iex> AdventOfCode.Day04.check_columns(
+      ...> [[[8, true], [2, false], [23, false]],
+      ...> [[4, false], [24, false], [25, false]],
+      ...> [[8, true], [2, false], [23, false]],
+      ...> [[4, true], [24, false], [25, false]]])
+      false
+
+  """
+  def check_columns(board) do
+    board |> Enum.zip() |> Enum.map(&Tuple.to_list/1) |> check_rows()
+  end
+
+  # def check_columns(board) do
+  #   firsts = Enum.reduce(data, [], fn
+  #     [{_, true} | tail], acc ->
+  #       [true | acc]
+  #     _ ->
+  #       [acc]
+  #   end)
+
+  #   [Enum.count(board) == Enum.count(firsts) | check_columns()]
+  # end
+
+
+
+  defp numbers_and_boards([n | b]) do
+    numbers = String.split(n, ",")
+      |> Enum.map(&String.to_integer/1)
+    boards = Enum.chunk_every(b, 5)
+      |> Enum.map(fn board ->
+        Enum.map(board, fn row ->
+          String.split(row, "\s", trim: true)
+          |> Enum.map(fn number -> [String.to_integer(number), false] end)
+        end)
+      end)
+
+
+    {numbers, boards}
   end
 
   def part2(args) do
