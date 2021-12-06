9426b73c577a6e5d81240b994975d6741d5aa3bb
Mackenzie Morgan <macoafi@gmail.com>
2021-12-06 01:05:33 -0500

    commit day 6 part 1 in case so at least that's saved

--- 2021/elixir/lib/advent_of_code/day_06.ex
+++ 2021/elixir/lib/advent_of_code/day_06.ex
@@ -1,7 +1,20 @@
 defmodule AdventOfCode.Day06 do
   def part1(args) do
+    fish = parse(args)
+    Enum.reduce(1..80, fish, fn _day, fishes ->
+      Enum.map(fishes, fn
+        0 ->
+          [6, 8]
+        x ->
+          x - 1
+      end)
+      |> List.flatten()
+    end)
+    |> Enum.count()
   end
 
   def part2(args) do
   end
+
+  defp parse(input), do: String.trim(input) |> String.split(",") |> Enum.map(&String.to_integer/1)
 end
