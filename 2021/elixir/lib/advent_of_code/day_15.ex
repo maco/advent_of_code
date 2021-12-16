1c1a49658b205b7f2f8e169cc0c83b78f37b7bbd
Mackenzie Morgan <macoafi@gmail.com>
2021-12-15 19:41:00 -0500

    add board expansion functions for part 2

--- 2021/elixir/lib/advent_of_code/day_15.ex
+++ 2021/elixir/lib/advent_of_code/day_15.ex
@@ -46,6 +46,41 @@ defmodule AdventOfCode.Day15 do
   def part2(args) do
   end
 
+  def expand_board(board, {max_row, max_col}) do
+    row1 = make_row(board, {max_row, max_col})
+    Enum.reduce(1..4, {row1, row1}, fn boards_down, {last_row, expanded} ->
+      next_row = bump_risk(last_row)
+      more_row = Enum.reduce(next_row, %{}, fn {{r, c}, v}, acc ->
+        Map.put(acc, {r + (boards_down * max_row), c}, v)
+      end)
+      |> Map.merge(expanded)
+      {next_row, more_row}
+    end)
+    |> elem(1)
+  end
+
+  def make_row(board, {_max_row, max_col}) do
+    Enum.reduce(1..4, {board, board}, fn boards_across, {last_board, expanded} ->
+      next_board = bump_risk(last_board)
+      more_row = Enum.reduce(next_board, %{}, fn {{r, c}, v}, acc ->
+        Map.put(acc, {r, c + (boards_across * max_col)}, v)
+      end)
+      |> Map.merge(expanded)
+      {next_board, more_row}
+    end)
+    |> elem(1)
+  end
+
+  def bump_risk(board) do
+    Map.map(board, fn {_, {v, d}} ->
+      new_v = case v do
+        9 -> 1
+        n -> n + 1
+      end
+      {new_v, d}
+    end)
+  end
+
   def parse(input) do
     # José did this in day 9, and it's so much tidier than reduce inside reduce.
     # I need to remember this trick.
@@ -55,5 +90,17 @@ defmodule AdventOfCode.Day15 do
         into: %{} do
       {{row, col}, {number - ?0, :infinity}}
     end
+  end
+
+  def show_grid(grid) do
+    keys = Map.keys(grid) |> Enum.sort()
+    for {_row, col} = k <- keys do
+      if col == 0 do
+        IO.write("\n")
+      end
+      {digit, _} = Map.get(grid, k, '*')
+      IO.write(digit)
+    end
+    grid
   end
 end
