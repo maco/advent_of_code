6662db7c313b1bc5a03d4d24d5d5481ef788faee
Mackenzie Morgan <macoafi@gmail.com>
2021-12-17 00:43:40 -0500

    remove debug code. format

--- 2021/elixir/lib/advent_of_code/day_16.ex
+++ 2021/elixir/lib/advent_of_code/day_16.ex
@@ -6,18 +6,11 @@ defmodule AdventOfCode.Day16 do
   end
 
   def decode_packet(<<v::3, 4::3, rest::bits>>) do
-    IO.puts("decode")
-    IO.inspect({bits_to_integer(<<v>>),4}, label: "{v, t}")
-    IO.puts("going to literal")
     {payload, remainder} = literal_value(rest)
     {%{version: v, type: 4, payload: payload}, remainder}
   end
 
   def decode_packet(<<v::3, t::3, rest::bits>>) do
-    IO.puts("decode")
-    IO.inspect({bits_to_integer(<<v>>),bits_to_integer(<<t>>)}, label: "{v, t}")
-    IO.puts("going to operator")
-    IO.inspect(bit_size(rest), label: "bits remaining")
     {payload, remainder} = operator(rest)
     {%{version: v, type: t, payload: payload}, remainder}
   end
@@ -27,73 +20,50 @@ defmodule AdventOfCode.Day16 do
       {2021, <<0::3>>}
   """
   def literal_value(bitlist) do
-    # IO.puts("entering literal")
     {v, remainder} = do_literal_value(bitlist)
 
     size = bit_size(v)
     <<int::size(size)>> = v
-    int |> IO.inspect(label: "literal")
-    IO.inspect(bit_size(remainder), label: "bits remaining")
 
     {int, remainder}
   end
 
   defp do_literal_value(<<0::1, b::4, rest::bits>>) do
-    # IO.puts("literal base")
     {<<b::4>>, rest}
   end
 
   defp do_literal_value(<<_::1, b::4, rest::bits>>) do
-    # IO.puts("literal recursion")
     {n, remainder} = do_literal_value(rest)
     m = <<b::4>>
-    # IO.inspect(is_bitstring(m), label: "m is bitstring")
-    # IO.inspect(bit_size(m), label: "m bit size")
-    # IO.inspect(bit_size(<< m::bits , n::bits >>), label: "full bit size")
-    {<< m::bits , n::bits >>, remainder}
+    {<<m::bits, n::bits>>, remainder}
   end
 
-  def operator(<<0::1, len::integer-size(15), rest::bits>> = bitlist) do
+  def operator(<<0::1, len::integer-size(15), rest::bits>>) do
     # IO.puts("operator")
-    IO.puts("by length")
-    IO.inspect(len, label: "bits in subpackets")
-    bit_size(rest) |> IO.inspect(label: "remaining bits")
     <<subpackets::size(len)-bits, remainder::bits>> = rest
-    {subpackets_by_bits(subpackets), remainder} |> IO.inspect(label: "data from subpacket by bit recursion")
+    {subpackets_by_bits(subpackets), remainder}
   end
 
-  def operator(<<1::1, count::11, rest::bits>> = bitlist) do
+  def operator(<<1::1, count::integer-size(11), rest::bits>>) do
     # IO.puts("operator")
-    IO.puts("by number of packets")
-    IO.inspect(count, label: "count")
-    IO.inspect(bit_size(bitlist), label: "bitlist size")
-    IO.inspect(bit_size(rest), label: "subpacket size")
-    {data, rem} = subpackets_by_count(rest, count)
-    data |> IO.inspect(label: "data from #{count} packets")
-    {data, rem}
+    subpackets_by_count(rest, count)
   end
 
   def subpackets_by_count(bitlist, 0), do: {[], bitlist}
 
   def subpackets_by_count(bitlist, count) do
-    IO.inspect(count, label: "subpacket")
     {data, rest} = decode_packet(bitlist)
-    IO.inspect(data, label: "data in subpacket")
-    # IO.inspect(rest, label: "rest of decoded subpacket")
     {subdata, remainder} = subpackets_by_count(rest, count - 1)
-    {[ data | subdata], remainder}
+    {[data | subdata], remainder}
   end
 
   def subpackets_by_bits(<<>>) do
-    IO.puts("ran out of bits")
-     []
+    []
   end
 
   def subpackets_by_bits(bitlist) do
-    IO.puts("inspecting a subpacket")
-    IO.inspect(bit_size(bitlist), label: "bit size of input")
-    {data, remainder} = decode_packet(bitlist) |> IO.inspect(label: "decoded subpacket")
-    [ data | subpackets_by_bits(remainder)]
+    {data, remainder} = decode_packet(bitlist)
+    [data | subpackets_by_bits(remainder)]
   end
 
   @doc """
@@ -106,18 +76,12 @@ defmodule AdventOfCode.Day16 do
       iex> AdventOfCode.Day16.total_versions(packet)
       9
   """
-  def total_versions(%{version: v, payload: x } = packet) when is_integer(x), do: v
+  def total_versions(%{version: v, payload: x}) when is_integer(x), do: v
 
-  def total_versions(%{version: v, payload: payload} = packet) do
+  def total_versions(%{version: v, payload: payload}) do
     Enum.reduce(payload, v, fn subpacket, acc ->
       acc + total_versions(subpacket)
     end)
-  end
-
-  defp bits_to_integer(bs) do
-    size = bit_size(bs)
-    <<int::size(size)>> = bs
-    int
   end
 
   def part2(args) do
