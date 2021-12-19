87d275747ab387853abed672fb01f6a9ae72f761
Mackenzie Morgan <macoafi@gmail.com>
2021-12-19 00:00:57 -0500

    explosions found but not unraveled

--- 2021/elixir/lib/advent_of_code/day_18.ex
+++ 2021/elixir/lib/advent_of_code/day_18.ex
@@ -22,21 +22,79 @@ defmodule AdventOfCode.Day18 do
   end
 
   @doc """
-      iex> AdventOfCode.Day18.try_explosion([[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]])
-      [[[[0,7],4],[7,[[8,4],9]]],[1,1]]
+      iex> AdventOfCode.Day18.try_explosion([[[[[9,8],1],2],3],4])
+      [[[[0,9],2],3],4]
+
+      # iex> AdventOfCode.Day18.try_explosion([7,[6,[5,[4,[3,2]]]]])
+      # [7,[6,[5,[7,0]]]]
+
+      # iex> AdventOfCode.Day18.try_explosion([[6,[5,[4,[3,2]]]],1])
+      # [[6,[5,[7,0]]],3]
+
+      # iex> AdventOfCode.Day18.try_explosion([[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]])
+      # [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]
+
+      # iex> AdventOfCode.Day18.try_explosion([[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]])
+      # [[3,[2,[8,0]]],[9,[5,[7,0]]]]
+
+      # iex> AdventOfCode.Day18.try_explosion([[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]])
+      # [[[[0,7],4],[7,[[8,4],9]]],[1,1]]
   """
   def try_explosion(sn) do
-    do_try_explode(sn, 3)
+    do_try_explode(sn, 1)
   end
 
-  def do_try_explode(sn, 0) do
-    Enum.reduce_while(sn, sn, fn [l, r], pairs ->
-      
-    end)
+  def do_try_explode([[l,r] | []], depth) do
+    IO.inspect("bottomed out")
+    IO.inspect(depth, label: "depth")
+    [l, r]
   end
 
-  def do_try_explode(sn, count) do
+  def do_try_explode([h | []], depth) do
+    IO.inspect("no tail")
+    IO.inspect(depth, label: "depth")
+    case do_try_explode(h, depth) do
+      {:explode, %{left: l, right: r}} ->
+        {}
+      n ->
+        n
+    end
+  end
 
+  def do_try_explode([l, r], 5)  when is_integer(l) and is_integer(r) do
+    IO.puts("level 4")
+    IO.inspect([l,r], label: "4-deep pair")
+    # IO.inspect(sn, label: "sn")
+    # [l, r] = Enum.find(sn, fn x -> is_list(x) end) |> IO.inspect(label: "explode this")
+    {:explode, %{left: l, right: r}}
+  end
+
+  def do_try_explode([l, r], depth) when is_integer(l) and is_integer(r) do
+    IO.inspect([l, r], label: "2 integers")
+    IO.inspect(depth, label: "depth")
+    [l, r]
+  end
+
+  def do_try_explode([h | t], depth) when is_integer(h) and is_integer(t) do
+    IO.inspect([h | t], label: "integers")
+    IO.inspect(depth, label: "depth")
+    [h | t]
+  end
+
+
+
+  def do_try_explode([h | t], depth) when is_integer(h) do
+    IO.inspect(h, label: "h")
+    IO.inspect(t, label: "with list")
+    IO.inspect(depth, label: "depth")
+    [h | do_try_explode(t, depth + 1)]
+  end
+
+  def do_try_explode([h | t], depth) do
+    IO.inspect(h, label: "h")
+    IO.inspect(t, label: "t")
+    IO.inspect(depth, label: "depth")
+    do_try_explode(h, depth + 1)
   end
 
   @doc """
@@ -52,31 +110,31 @@ defmodule AdventOfCode.Day18 do
   def try_split([h, t] = sn)
   when is_integer(h) and h < 10 and is_integer(t) and t < 10
   do
-    IO.inspect(sn, label: "h and t < 10")
+    # IO.inspect(sn, label: "h and t < 10")
     sn
   end
 
   def try_split([h | t] = n) when is_integer(h) and h >= 10 do
-    IO.inspect(n, label: "with h >= 10")
+    # IO.inspect(n, label: "with h >= 10")
     left = floor(h/2)
     right = ceil(h/2)
     [[left, right] | t]
   end
 
   def try_split([h | t] = n) when is_integer(t) and t >= 10 do
-    IO.inspect(n, label: "with t >= 10")
+    # IO.inspect(n, label: "with t >= 10")
     left = floor(t/2)
     right = ceil(t/2)
     [h | [left, right]]
   end
 
   def try_split([h | t] = n) when is_integer(h) do
-    IO.inspect(n, label: "with h integer")
+    # IO.inspect(n, label: "with h integer")
     [h | try_split(t)]
   end
 
   def try_split([h | t] = n) when is_list(h) do
-    IO.inspect(n, label: "with h list")
+    # IO.inspect(n, label: "with h list")
     case try_split(h) do
       ^h -> [h | try_split(t)]
       new -> [new | t]
@@ -85,12 +143,12 @@ defmodule AdventOfCode.Day18 do
   end
 
   def try_split([h | t]) when is_list(t) do
-    IO.inspect(label: "when t list")
+    # IO.inspect(label: "when t list")
     [h | try_split(t)]
   end
 
   def try_split(sn) do
-    IO.inspect(sn, label: "hmm")
+    # IO.inspect(sn, label: "hmm")
   end
 
   def part2(args) do
