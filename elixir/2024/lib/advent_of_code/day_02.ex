c578b86e3193f70037f3643b4926164d0fa1ec45
Mackenzie Morgan <macoafi@gmail.com>
2024-12-02 15:36:42 -0500

    refactoring

--- elixir/2024/lib/advent_of_code/day_02.ex
+++ elixir/2024/lib/advent_of_code/day_02.ex
@@ -4,7 +4,7 @@ defmodule AdventOfCode.Day02 do
   def part1(input) do
     input
     |> parse_rows_ints
-    |> Enum.count(fn row -> safe_increasing(row) or safe_decreasing(row) end)
+    |> Enum.count(fn row -> safe_changing(:inc, row) or safe_changing(:dec, row) end)
   end
 
   def part2(input) do
@@ -14,15 +14,11 @@ defmodule AdventOfCode.Day02 do
     end)
   end
 
-  defp safe_increasing([_]), do: true
-  defp safe_increasing([a, b | _tail]) when a >= b, do: false
-  defp safe_increasing([a, b | _tail]) when b - a > 3, do: false
-  defp safe_increasing([_a, b | tail]), do: safe_increasing([b | tail])
-
-  defp safe_decreasing([_]), do: true
-  defp safe_decreasing([a, b | _tail]) when a <= b, do: false
-  defp safe_decreasing([a, b | _tail]) when a - b > 3, do: false
-  defp safe_decreasing([_a, b | tail]), do: safe_decreasing([b | tail])
+  defp safe_changing(_, [_]), do: true
+  defp safe_changing(:inc, [a, b | _tail]) when a >= b, do: false
+  defp safe_changing(:dec, [a, b | _tail]) when a <= b, do: false
+  defp safe_changing(_, [a, b | _tail]) when abs(b - a) > 3, do: false
+  defp safe_changing(direction, [_a, b | tail]), do: safe_changing(direction, [b | tail])
 
   defp safe_ish_increasing([_], _), do: true
 
@@ -30,11 +26,11 @@ defmodule AdventOfCode.Day02 do
     do: safe_ish_increasing([b | tail], [a | popped])
 
   defp safe_ish_increasing([a, b | tail] = _list, []) do
-    safe_increasing([a | tail]) or safe_increasing([b | tail])
+    safe_changing(:inc, [a | tail]) or safe_changing(:inc, [b | tail])
   end
 
   defp safe_ish_increasing([a, b | tail] = _list, [last | _]) do
-    safe_increasing([last, a | tail]) or safe_increasing([last, b | tail])
+    safe_changing(:inc, [last, a | tail]) or safe_changing(:inc, [last, b | tail])
   end
 
   defp safe_ish_decreasing([_], _), do: true
@@ -43,10 +39,10 @@ defmodule AdventOfCode.Day02 do
     do: safe_ish_decreasing([b | tail], [a | popped])
 
   defp safe_ish_decreasing([a, b | tail], []) do
-    safe_decreasing([a | tail]) or safe_decreasing([b | tail])
+    safe_changing(:dec, [a | tail]) or safe_changing(:dec, [b | tail])
   end
 
   defp safe_ish_decreasing([a, b | tail], [last | _]) do
-    safe_decreasing([last, a | tail]) or safe_decreasing([last, b | tail])
+    safe_changing(:dec, [last, a | tail]) or safe_changing(:dec, [last, b | tail])
   end
 end
