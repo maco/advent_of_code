95ea92d5c78e8a06b7f46680533c4db5d3658a3d
Mackenzie Morgan <macoafi@gmail.com>
2024-12-02 18:54:52 -0500

    day 2: refactor to use first class functions

--- python/solutions/2024/day_02/solution.py
+++ python/solutions/2024/day_02/solution.py
@@ -4,72 +4,58 @@
 
 from ...base import StrSplitSolution, answer
 from collections import deque
+from collections.abc import Callable
 
 
 class Solution(StrSplitSolution):
     _year = 2024
     _day = 2
 
-    # @answer(1234)
+    @answer(637)
     def part_1(self) -> int:
         levels: list[deque[int]] = []
         for row in self.input:
             levels.append(deque(map(int, row.split())))
 
         safe_levels = list(filter(lambda row:
-                             self.__safe_decreasing(row) or self.__safe_increasing(row),
+                             self.__safe_changing(row, self.__safe_decrease) or self.__safe_changing(row, self.__safe_increase),
                              levels))
         return len(safe_levels)
 
-
-    # @answer(1234)
+    @answer(665)
     def part_2(self) -> int:
         levels: list[deque[int]] = []
         for row in self.input:
             levels.append(deque(map(int, row.split())))
 
-        print(levels)
         safe_levels = list(filter(lambda row:
-                             self.__safe_ish_decreasing(row.copy(), []) or self.__safe_ish_increasing(row.copy(), []),
+                             self.__safe_ish_changing(row.copy(), [], self.__safe_decrease) or self.__safe_ish_changing(row.copy(), [], self.__safe_increase),
                              levels))
         return len(safe_levels)
 
+    def __safe_changing(self, values: deque[int], comparator: Callable) -> bool:
+        last = values.popleft()
+        for val in values:
+            if not comparator(last, val):
+                return False
+            last = val
+        return True
 
-
-    def __safe_ish_decreasing(self, values: deque[int], seen: list[int]) -> bool:
+    def __safe_ish_changing(self, values: deque[int], seen: list[int], comparator: Callable) -> bool:
         try:
             a = values.popleft()
             b = values.popleft()
-            if b < a and a - b <= 3:
+            if comparator(a, b):
                 seen.append(a)
                 values.appendleft(b)
-                return self.__safe_ish_decreasing(values, seen)
+                return self.__safe_ish_changing(values, seen, comparator)
             elif seen == []:
-                return self.__safe_decreasing(self.__append_head(values, a)) \
-                    or self.__safe_decreasing(self.__append_head(values, b))
+                return self.__safe_changing(self.__append_head(values, a), comparator) \
+                    or self.__safe_changing(self.__append_head(values, b), comparator)
             else:
                 last = seen.pop()
-                return self.__safe_decreasing(self.__append_head(self.__append_head(values, a), last)) \
-                    or self.__safe_decreasing(self.__append_head(self.__append_head(values, b), last))
-
-        except IndexError:
-            return True
-
-    def __safe_ish_increasing(self, values: deque[int], seen: list[int]) -> bool:
-        try:
-            a = values.popleft()
-            b = values.popleft()
-            if b > a and b - a <= 3:
-                seen.append(a)
-                values.appendleft(b)
-                return self.__safe_ish_increasing(values, seen)
-            elif seen == []:
-                return self.__safe_increasing(self.__append_head(values, a)) \
-                    or self.__safe_increasing(self.__append_head(values, b))
-            else:
-                last = seen.pop()
-                return self.__safe_increasing(self.__append_head(self.__append_head(values, a), last)) \
-                    or self.__safe_increasing(self.__append_head(self.__append_head(values, b), last))
+                return self.__safe_changing(self.__append_head(self.__append_head(values, a), last), comparator) \
+                    or self.__safe_changing(self.__append_head(self.__append_head(values, b), last), comparator)
 
         except IndexError:
             return True
@@ -79,22 +65,8 @@ class Solution(StrSplitSolution):
         my_copy.appendleft(head)
         return my_copy
 
-    def __safe_decreasing(self, values: deque[int]) -> bool:
-        last = values.popleft()
-        for val in values:
-            if last <= val:
-                return False
-            elif last - val > 3:
-                return False
-            last = val
-        return True
+    def __safe_increase(self, val1: int, val2: int) -> bool:
+        return val1 < val2 and val2 - val1 <= 3
 
-    def __safe_increasing(self, values: deque[int]) -> bool:
-        last = values.popleft()
-        for val in values:
-            if last >= val:
-                return False
-            elif  val - last > 3:
-                return False
-            last = val
-        return True
+    def __safe_decrease(self, val1: int, val2: int) -> bool:
+        return val1 > val2 and val1 - val2 <= 3
