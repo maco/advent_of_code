6794263103adfa25382f28bfe8a09a00e81b4b1d
Mackenzie Morgan <macoafi@gmail.com>
2024-12-03 12:07:16 -0500

    formatting

--- python/solutions/2024/day_03/solution.py
+++ python/solutions/2024/day_03/solution.py
@@ -12,27 +12,25 @@ class Solution(TextSolution):
 
     @answer(161289189)
     def part_1(self) -> int:
-        captures = re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', self.input)
+        captures = re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", self.input)
         total = 0
-        for (x, y) in captures:
+        for x, y in captures:
             total += int(x) * int(y)
         return total
 
     @answer(83595109)
     def part_2(self) -> int:
-        captures = re.findall(r'(mul)\((\d{1,3}),(\d{1,3})\)|(don\'t\(\))|(do\(\))', self.input)
+        captures = re.findall(
+            r"(mul)\((\d{1,3}),(\d{1,3})\)|(don\'t\(\))|(do\(\))", self.input
+        )
         total = 0
         enabled = True
         for capture in captures:
             match capture:
-                case ('mul', x, y, _, _):
+                case ("mul", x, y, _, _):
                     total += int(x) * int(y) if enabled else 0
-                case(_, _, _, "don't()", _):
+                case (_, _, _, "don't()", _):
                     enabled = False
-                case(_, _, _, _, "do()"):
+                case (_, _, _, _, "do()"):
                     enabled = True
         return total
-
-    # @answer((1234, 4567))
-    # def solve(self) -> tuple[int, int]:
-    #     pass
