9125b68f9d577ebd62f010003441f06c6908e07e
Mackenzie Morgan <macoafi@gmail.com>
2024-12-04 19:52:22 -0500

    DRY

--- elixir/2024/lib/advent_of_code/day_04.ex
+++ elixir/2024/lib/advent_of_code/day_04.ex
@@ -1,22 +1,30 @@
 defmodule AdventOfCode.Day04 do
   import AdventOfCode.Parsing
 
-  @directions [:north, :south, :east, :west, :northeast, :southeast, :northwest, :southwest]
-
   def part1(input) do
+    operations = [
+      {&Kernel.+/2, &Kernel.+/2},
+      {&Kernel.-/2, &Kernel.-/2},
+      {&Kernel.+/2, &Kernel.-/2},
+      {&Kernel.-/2, &Kernel.+/2},
+      {&nop/2, &Kernel.+/2},
+      {&nop/2, &Kernel.-/2},
+      {&Kernel.+/2, &nop/2},
+      {&Kernel.-/2, &nop/2}
+    ]
+
     grid = parse_grid(input)
     x_locations = Map.filter(grid, fn {_key, val} -> val == "X" end)
 
     Enum.reduce(x_locations, 0, fn {x_pos, _}, total ->
-      xmases =
-        Enum.count(@directions, fn direction ->
-          find_neighbors(grid, x_pos, direction)
+      xmases2 =
+        Enum.count(operations, fn operation ->
+          find_neighbors(grid, x_pos, operation)
           |> is_xmas?
         end)
 
-      total + xmases
+      total + xmases2
     end)
-    |> IO.inspect()
   end
 
   def part2(_args) do
@@ -25,67 +33,12 @@ defmodule AdventOfCode.Day04 do
   defp is_xmas?({"M", "A", "S"}), do: true
   defp is_xmas?(_), do: false
 
-  def find_neighbors(grid, {x_row, x_col}, :east) do
-    {
-      Map.get(grid, {x_row, x_col + 1}, :not_found),
-      Map.get(grid, {x_row, x_col + 2}, :not_found),
-      Map.get(grid, {x_row, x_col + 3}, :not_found)
-    }
-  end
+  def nop(val, _), do: val
 
-  def find_neighbors(grid, {x_row, x_col}, :west) do
-    {
-      Map.get(grid, {x_row, x_col - 1}, :not_found),
-      Map.get(grid, {x_row, x_col - 2}, :not_found),
-      Map.get(grid, {x_row, x_col - 3}, :not_found)
-    }
-  end
-
-  def find_neighbors(grid, {x_row, x_col}, :south) do
-    {
-      Map.get(grid, {x_row + 1, x_col}, :not_found),
-      Map.get(grid, {x_row + 2, x_col}, :not_found),
-      Map.get(grid, {x_row + 3, x_col}, :not_found)
-    }
-  end
-
-  def find_neighbors(grid, {x_row, x_col}, :north) do
-    {
-      Map.get(grid, {x_row - 1, x_col}, :not_found),
-      Map.get(grid, {x_row - 2, x_col}, :not_found),
-      Map.get(grid, {x_row - 3, x_col}, :not_found)
-    }
-  end
-
-  def find_neighbors(grid, {x_row, x_col}, :southeast) do
-    {
-      Map.get(grid, {x_row + 1, x_col + 1}, :not_found),
-      Map.get(grid, {x_row + 2, x_col + 2}, :not_found),
-      Map.get(grid, {x_row + 3, x_col + 3}, :not_found)
-    }
-  end
-
-  def find_neighbors(grid, {x_row, x_col}, :southwest) do
-    {
-      Map.get(grid, {x_row + 1, x_col - 1}, :not_found),
-      Map.get(grid, {x_row + 2, x_col - 2}, :not_found),
-      Map.get(grid, {x_row + 3, x_col - 3}, :not_found)
-    }
-  end
-
-  def find_neighbors(grid, {x_row, x_col}, :northwest) do
-    {
-      Map.get(grid, {x_row - 1, x_col - 1}, :not_found),
-      Map.get(grid, {x_row - 2, x_col - 2}, :not_found),
-      Map.get(grid, {x_row - 3, x_col - 3}, :not_found)
-    }
-  end
-
-  def find_neighbors(grid, {x_row, x_col}, :northeast) do
-    {
-      Map.get(grid, {x_row - 1, x_col + 1}, :not_found),
-      Map.get(grid, {x_row - 2, x_col + 2}, :not_found),
-      Map.get(grid, {x_row - 3, x_col + 3}, :not_found)
-    }
+  def find_neighbors(grid, {x_row, x_col}, {op1, op2}) do
+    Enum.map(1..3, fn offset ->
+      Map.get(grid, {op1.(x_row, offset), op2.(x_col, offset)}, :not_found)
+    end)
+    |> List.to_tuple()
   end
 end
