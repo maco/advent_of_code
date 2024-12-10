dda45db6c34d59ee4d22a4d328e0ef6be78c01e2
Mackenzie Morgan <macoafi@gmail.com>
2024-12-10 13:27:15 -0500

    refactor

--- python/solutions/2024/day_10/solution.py
+++ python/solutions/2024/day_10/solution.py
@@ -14,31 +14,25 @@ class Solution(StrSplitSolution):
         self.topography = [[int(char) for char in line] for line in self.input]
         self.width = len(self.topography[0])
         self.height = len(self.topography)
+        self.paths = 0
         summits = []
-        paths = 0
         for row in range(self.height):
             for col in range(self.width):
                 if self.topography[row][col] == 0:
-                    found_summits, found_paths = self._find_summits(row, col, set(), 0)
+                    found_summits = set()
+                    self._find_summits(row, col, found_summits)
                     summits.extend(found_summits)
-                    paths += found_paths
 
-        return (len(summits), paths)
+        return (len(summits), self.paths)
 
-    def _find_summits(
-        self, row: int, col: int, summits: set[tuple[int, int]], paths: int
-    ) -> tuple[set[tuple[int, int]], int]:
+    def _find_summits(self, row: int, col: int, summits: set[tuple[int, int]]) -> None:
         if self.topography[row][col] == 9:
             summits.add((row, col))
-            paths += 1
-            return (summits, paths)
-
-        for next_row, next_col in self._list_neighbors(row, col):
-            if self.topography[next_row][next_col] - self.topography[row][col] == 1:
-                (summits, paths) = self._find_summits(
-                    next_row, next_col, summits, paths
-                )
-        return (summits, paths)
+            self.paths += 1
+        else:
+            for next_row, next_col in self._list_neighbors(row, col):
+                if self.topography[next_row][next_col] - self.topography[row][col] == 1:
+                    self._find_summits(next_row, next_col, summits)
 
     def _list_neighbors(self, row, col):
         neighbors = []
