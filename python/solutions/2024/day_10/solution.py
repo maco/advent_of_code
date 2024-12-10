c2338275520d914e983aa5ddfc0e996c70bf7448
Mackenzie Morgan <macoafi@gmail.com>
2024-12-10 13:19:48 -0500

    day 10 part 2 python

--- python/solutions/2024/day_10/solution.py
+++ python/solutions/2024/day_10/solution.py
@@ -9,33 +9,36 @@ class Solution(StrSplitSolution):
     _year = 2024
     _day = 10
 
-    @answer(820)
-    def part_1(self) -> int:
+    @answer((820, 1786))
+    def solve(self) -> tuple[int, int]:
         self.topography = [[int(char) for char in line] for line in self.input]
         self.width = len(self.topography[0])
         self.height = len(self.topography)
         summits = []
+        paths = 0
         for row in range(self.height):
             for col in range(self.width):
                 if self.topography[row][col] == 0:
-                    summits.extend(self._find_summits(row, col, set()))
-        return len(summits)
+                    found_summits, found_paths = self._find_summits(row, col, set(), 0)
+                    summits.extend(found_summits)
+                    paths += found_paths
 
-    # @answer(1234)
-    def part_2(self) -> int:
-        pass
+        return (len(summits), paths)
 
     def _find_summits(
-        self, row: int, col: int, acc: set[tuple[int, int]]
-    ) -> list[tuple[int, int]]:
+        self, row: int, col: int, summits: set[tuple[int, int]], paths: int
+    ) -> tuple[set[tuple[int, int]], int]:
         if self.topography[row][col] == 9:
-            acc.add((row, col))
-            return acc
+            summits.add((row, col))
+            paths += 1
+            return (summits, paths)
 
         for next_row, next_col in self._list_neighbors(row, col):
             if self.topography[next_row][next_col] - self.topography[row][col] == 1:
-                acc = self._find_summits(next_row, next_col, acc)
-        return acc
+                (summits, paths) = self._find_summits(
+                    next_row, next_col, summits, paths
+                )
+        return (summits, paths)
 
     def _list_neighbors(self, row, col):
         neighbors = []
@@ -48,7 +51,3 @@ class Solution(StrSplitSolution):
         if col < self.width - 1:
             neighbors.append((row, col + 1))
         return neighbors
-
-    # @answer((1234, 4567))
-    # def solve(self) -> tuple[int, int]:
-    #     pass
