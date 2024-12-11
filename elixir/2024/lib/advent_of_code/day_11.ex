07b358b4658257498d9e21f62ff6d19597a15046
Mackenzie Morgan <macoafi@gmail.com>
2024-12-11 11:56:06 -0500

    day 11 part 1 elixir

--- elixir/2024/lib/advent_of_code/day_11.ex
+++ elixir/2024/lib/advent_of_code/day_11.ex
@@ -1,7 +1,39 @@
 defmodule AdventOfCode.Day11 do
-  def part1(_args) do
+  def part1(input) do
+    stones = input |> String.split() |> Enum.map(&String.to_integer/1)
+
+    Enum.reduce(0..24, stones, fn _, acc ->
+      blink(acc)
+    end)
+    |> Enum.count()
   end
 
   def part2(_args) do
+  end
+
+  def blink(stones) do
+    Enum.reduce(stones, [], fn
+      0, acc ->
+        [1 | acc]
+
+      stone, acc ->
+        digits = Integer.digits(stone)
+        length = length(digits)
+
+        case rem(length, 2) do
+          0 ->
+            [left, right] =
+              digits
+              |> Enum.split(Integer.floor_div(length, 2))
+              |> Tuple.to_list()
+              |> Enum.map(&Integer.undigits/1)
+
+            [right, left | acc]
+
+          _ ->
+            [stone * 2024 | acc]
+        end
+    end)
+    |> Enum.reverse()
   end
 end
--- elixir/2024/test/advent_of_code/day_11_test.exs
+++ elixir/2024/test/advent_of_code/day_11_test.exs
@@ -3,12 +3,14 @@ defmodule AdventOfCode.Day11Test do
 
   import AdventOfCode.Day11
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  test "blink once" do
+    input = [0, 1, 10, 99, 999]
+    [1, 2024, 1, 0, 9, 9, 2_021_976] = blink(input)
+  end
 
-    assert result
+  test "part1" do
+    input = "125 17"
+    55312 = part1(input)
   end
 
   @tag :skip
